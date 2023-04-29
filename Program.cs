290ef3e33a631685e300a2ef641b8edfc17a4d78
Majd-Alshaqra <malshaqraa@smail.ucas.edu.ps>
2023-04-29 08:17:16 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+using cities;
+var builder = WebApplication.CreateBuilder(args);
+
+//Minimal API :
+var app = builder.Build();
+app.Urls.Add("http://localhost:5000");
+var Cars = new List<City>{
+    new City{
+        Id = 1,
+        city = "London",
+        Temperature = 11,
+        High = 200,
+        Low = 200,
+    },
+    new City{
+        Id = 1,
+        city = "Gaza",
+        Temperature = 22,
+        High = 200,
+        Low = 200,
+    },
+    new City{
+        Id = 1,
+        city = "Dubai",
+        Temperature = 33,
+        High = 200,
+        Low = 200,
+    },
+};
+app.MapGet("/api/cities/{id}", (string Id) =>City.cities.FirstOrDefault(c => 
+c.Id == Id)); // Get Car By Id
+// Update cities
+app.MapPut("/api/cities/{id}", (string id, City updatedCar) => 
+{
+var carIndex = City.cities.FindIndex(c => c.Id == Id);
+City.cities[carIndex] = updatedCar;
+return "Updated!";
+});
+
+app.Run();
---
+++ cities.cs
@@ -0,0 +1,10 @@
+namespace cities{
+
+public class City{
+        public int Id {get; set;}
+        public string city {get; set;}
+        public int Temperature {get; set;}
+        public int High {get; set;}
+        public int Low {get; set;}
+    }
+}
